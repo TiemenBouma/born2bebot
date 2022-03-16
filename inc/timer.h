36a4dccc06cc9345672d1441dfe78c04c502ce6d
Lars Verdoes <lverdoes@f0r2s13.codam.nl>
2022-03-16 14:54:16 +0100

    Peer++ timer

--- inc/timer.h
+++ inc/timer.h
@@ -7,17 +7,6 @@
 
 #define one_million 1000000
 
-typedef struct s_timer
-{
-	struct timeval	current_time;
-	size_t			start_s;
-	size_t			start_us;
-	size_t			elapsed_s;
-	size_t			elapsed_us;
-	size_t			timestamp;
-}	t_timer;
-
-/*		functions	*/
 void	timer(float timeout, volatile const bool* const end_of_turn);
 
 #endif
--- src/timer.c
+++ src/timer.c
@@ -4,32 +4,50 @@
 #include "timer.h"
 #include "born2bebot.h"
 
-static void	update_elapsed_time(t_timer *t, size_t time_limit_in_us)
+
+int    get_elapsed_time(struct timeval* result, struct timeval* x, struct timeval* y)
 {
-	gettimeofday(&t->current_time, NULL);
-	t->elapsed_us = t->current_time.tv_usec - t->start_us;
-	t->elapsed_s = t->current_time.tv_sec - t->start_s;
-	if (t->elapsed_s > 0)
-		t->elapsed_us += (t->elapsed_s * time_limit_in_us);
+    // preserve *y
+    struct timeval yy = *y;
+    y = &yy;
+
+    /* Perform the carry for the later subtraction by updating y. */  
+    if (x->tv_usec < y->tv_usec) {  
+        int nsec = (y->tv_usec - x->tv_usec) / one_million + 1;  
+        y->tv_usec -= one_million * nsec;  
+        y->tv_sec += nsec;  
+    }  
+    if (x->tv_usec - y->tv_usec > one_million) {  
+        int nsec = (y->tv_usec - x->tv_usec) / one_million;  
+        y->tv_usec += one_million * nsec;  
+        y->tv_sec -= nsec;  
+    }  
+    /* Compute the time remaining to wait.
+        tv_usec is certainly positive. */  
+    result->tv_sec = x->tv_sec - y->tv_sec;  
+    result->tv_usec = x->tv_usec - y->tv_usec;  
+
+    /* Return 1 if result is negative. */  
+    return x->tv_sec < y->tv_sec;  
 }
 
 void	timer(float timeout, volatile const bool* const end_of_turn)
 {
-	const size_t	nappytime = 100;
-	const size_t	time_limit_in_us = timeout * one_million;
-	t_timer			t;
+	const size_t	time_limit_in_us = timeout * one_million * 0.95;
+	struct timeval	start;
+	struct timeval	current;
+	struct timeval	diff;
 
-	bzero(&t, sizeof(t));
-	gettimeofday(&t.current_time, NULL);
-	t.start_s = t.current_time.tv_sec;
-	t.start_us = t.current_time.tv_usec;
-	while (t.elapsed_us + nappytime < time_limit_in_us)
-	{
+	gettimeofday(&start, NULL);
+    size_t elapsed_us = 0;
+ 
+    while (elapsed_us < time_limit_in_us)
+    {
 		if (*end_of_turn == true)
 			return;
-		usleep(nappytime);
-		update_elapsed_time(&t, time_limit_in_us);
-	}
-	dprintf(2, "---- time's up! A move has to be made now...\n");
-	dprintf(2, "time elapsed (us): [%lu] / [%lu]\n", t.elapsed_us, time_limit_in_us);
+        gettimeofday(&current, NULL);
+        get_elapsed_time(&diff, &current, &start);
+        elapsed_us = diff.tv_sec * one_million + diff.tv_usec;
+    }
+	dprintf(2, "time elapsed (us): [%lu] / [%lu]\n", elapsed_us, time_limit_in_us);
 }
